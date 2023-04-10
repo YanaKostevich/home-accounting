59d68d00bf68e2d0219fc4dd66c4a16dd27f3cb5
Jane Kostevich <yana.kostevich18@gmail.com>
2023-04-11 02:17:01 +0300

    pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <router-view/>
+  <component :is="layout">
+    <router-view/>
+  </component>
 </template>
 
-<style lang="scss">
+<script>
+import EmptyLayout from '@/layouts/EmptyLayout.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
 
+export default {
+  components: {
+    EmptyLayout,
+    MainLayout
+  },
+  computed: {
+    layout () {
+      return (this.$route.meta.layout || 'empty') + '-layout'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
 </style>
